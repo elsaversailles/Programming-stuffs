0aef7512a833ed510cab6e299e16fe1c34058952
Elsa Versailles <50725388+elsaversailles@users.noreply.github.com>
2020-12-14 09:12:20 +0800

    Add files via upload
    This  is the shortest version where readability isn't the priority. I use macros in order to keep the main code body as small as possible.
    This code is a side project. Ignore this/ :)

---
+++ short.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#define c  printf ("\n")
+#define b std::string
+#define d std::cin >>
+#define a cout<< 
+#define e <<
+using namespace std;
+int main() {
+	a"What's your First name? ";b f;
+	std::getline(cin, f); c; a"Nice! What's your Middle initial? "; b m; d m; c;
+	a"Awesome! Lastly, enter your Surname! "; b s; d s; c;
+	a "Good day! " + f e " " + m e ". " + s e + "\nHave a nice day! " + f + " ingat :)"; a endl; return 0;
+}
