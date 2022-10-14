553da975b35f079b956a89fee18fda8ab9ca3c6a
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-10-14 12:45:11 +0800

    Create currency_custom1.1.cpp

---
+++ currency_custom1.1.cpp
@@ -0,0 +1,157 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int amount; //user define
+	
+	int n1,n5,n10,n20,n50,n100,n200,n500,n1000,n1k,n500k,amount1,amount2,amount3,n200k,amount4,n100k,n50k,amount5,n20k,amount6,n10k,amount7,amount8,n5k;
+	
+	cout<<"Please Enter Your total Amount to find the notes : ";
+	cin>>amount;
+	
+	n1000 = amount/1000; //show if its whole number
+	//cout<<("\t");
+	//cout<<(n1000);
+	cout<<("\t \n \t");
+	
+	n1k = (n1000>=1); //compare if the value is divisible
+	cout<<("1000: ");
+	cout<<(n1000); //prints the total 1000 in value
+	
+	if (n1k >= 1){
+	amount1 = (amount - (n1000*1000)); //print the whole number remainder
+	}
+	
+	else {
+	amount1 = amount;
+	}
+	
+	n500 = amount1/500; //2
+	cout<< ("\n \t500: ");
+	cout<<(n500); //prints the total 500 in value
+	cout<<("\n");
+	
+	n500k = (n500>=1); //checkline
+	
+	if (n500k >= 1){
+	amount2 = (amount1 - (n500*500));	
+	} //nest this so compiler knows the end of statement
+	
+	else
+	amount2 = amount;
+	
+	n200 = amount2/200;
+	
+	n200k = (n200>=1);
+	cout<<("\t");
+	cout<<("200: ");
+	cout<<(n200k); //prints the total 200 in value
+	
+	if (n200k >= 1){
+	amount3 = (amount2 - (n200*200));
+	cout<<("\n");
+	}
+	
+	else
+	amount3 = amount;
+	
+	n100 = amount3/100;
+	
+	n100k = (n100>=1);
+	cout<<("\t");
+	cout<<("100: ");
+	cout<<(n100); //prints the total 100 in value
+	
+	if (n100k >= 1) {
+		amount4 = (amount3 - (n100*100)); //amount4 is the new value of amount if its divisbile by 100
+		cout<<("\n");
+	}
+	
+	else
+	amount4 = amount; //if the amount is not divisble by 100 it'll retain its value and be assigned a new one
+	
+	n50 = amount4/50;
+	
+	n50k = (n50>=1);
+	cout<<("\t");
+	cout<<("50: ");
+	cout<<(n50k); //prints the total 50 in value
+	
+	if (n50k >= 1) {
+		amount5 = (amount4 - (n50*50)); //amount5 is the new value of amount if its divisbile by 50
+		cout<<("\n");
+	}
+	
+	else
+	amount5 = amount;
+	
+	n20 = amount5/20;
+	n20k = (n20>=1);
+	
+	if (n20k >= 1) {
+		amount6 = (amount5 - (n20*20)); //amount6 is the new value of amount if its divisbile by 100
+		cout<<("\t20: ");
+		cout<<(n20); //prints the total 20 in value
+	}
+	
+	else  {				//compartmentalized
+	amount6 = amount;
+	cout<<("\t20: ");
+	cout<<n20k;
+}
+
+	n10 = amount6/10;
+	n10k = (n10>=1);
+	cout<<("\n");
+		
+	if (n10k >= 1) {
+		amount7 = (amount6 - (n10*10)); //amount7 is the new value of amount if its divisbile by 10
+		cout<<("\t10: ");
+		cout<<(n10); //prints the total 10 in value
+	}
+	
+	else{
+	amount7 = amount;
+	cout<<("\t10: ");
+	cout<<(n10k);
+}
+
+	n5 = amount7/5;
+	n5k = (n5>=1);
+	cout<<("\n");
+
+	if (n5 >= 2) {
+		cout<<("\t5: 0");
+		goto peso; //go to statement to circumvent a bug
+	}
+		
+	if (n5k == 1) {
+		amount8 = (amount7 - (n5*5)); //amount8 is the new value of amount if its divisbile by 5
+		cout<<("\t5: ");
+		cout<<(n5k); //prints the total 5 coins in value
+	}
+	
+	else{
+		amount8 = amount;
+		cout<<("\n 5: ");
+		cout<<(n5k);
+
+	}
+	
+peso:
+	amount8 = (amount7 - (n5*5));
+	n1 = amount8/1;
+	n1k = (n1>=1);
+	cout<<("\n \t");
+	
+		
+	if (amount8 >= 10){
+		cout<<("1: 0");
+		exit(0);
+	}
+	
+	else if (n1k >= 1) 
+		cout<<("1: ");
+		cout<<n1;
+
+}
