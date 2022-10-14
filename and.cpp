a5d0a6dd13f6db22a6f7ecf0ffa3f4500f2a290c
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-10-14 12:41:54 +0800

    Create and.cpp

---
+++ and.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=16, b=3, c=2, d=1;
+	
+	cout<< ("\n \t");
+	cout << (c==2) && (a<2);
+	
+	//this returns 1 (true)
+	
+	return 0;
+}
