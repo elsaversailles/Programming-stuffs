79522b3b3c981ddab086f7dd5dbc834533531f28
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-10-15 07:36:14 +0800

    Update and.cpp

--- and.cpp
+++ and.cpp
@@ -6,9 +6,14 @@ int main()
 	int a=16, b=3, c=2, d=1;
 	
 	cout<< ("\n \t");
-	cout << (c==2) && (a<2);
-	
+	cout << ("not fix precedemce ");
+	cout << (c==2) && (a<2); 
 	//this returns 1 (true)
+	
+	cout << ("precedence fix ");
+	cout << ((c==2) && (a<2));
+	//this fixes precedence issue
+	
 	
 	return 0;
 }
