c9bf2a01777f3e74bc5e73c21d40c35809ad6a97
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-10-15 08:29:46 +0800

    Create else2.cpp

---
+++ else2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int x, result;
+cout<<"Enter number: ";
+    cin>>x;
+    //check if the number is odd or even
+    if(x%2==0) {
+        cout<<"Even number";
+    }
+    else
+        cout<<"Odd number";
+    
+        
+}
