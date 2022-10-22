5272616c791dc63b23e2ea816495fa80af2f92be
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-10-22 09:36:27 +0800

    Update tax.cpp

--- tax.cpp
+++ tax.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int single, married, widow, hof, salary, tax, medicare, sss, monthly_income, deduction, total_deduction, dependents,
-    gross_income, exemption, net_monthly_income, annual_tax_income, tax_due; //single, married, widow, hof = array
+    gross_income, exemption, net_monthly_income, annual_tax_income, tax_due, antaxinc; //single, married, widow, hof = array
 
     string lastname, firstname, marital_status;
     cout << "Enter your lastname: ";
@@ -62,6 +62,103 @@ int main()
            }
            else if (annual_tax_income >= 250001 && annual_tax_income <= 400000)
            {
+               tax_due = (annual_tax_income - 250000) * 0.15;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income == 400000)
+           {
+               tax_due = annual_tax_income - 22500;
+               cout << "Tax due: " << tax_due << endl;
+ 
+           }
+           else if (annual_tax_income >= 400001 && annual_tax_income <= 800000)
+           {
+                tax_due = (annual_tax_income - 400000);
+               // tax_due = 192,800
+               tax_due = (tax_due * 0.20) + 22500 ;
+               cout << "Tax due: " << tax_due << endl;
+ 
+           }
+
+         else if (annual_tax_income == 800000)
+           {
+               tax_due = annual_tax_income - 102500;
+               cout << "Tax due: " << tax_due << endl;
+ 
+           }
+
+
+           else if (annual_tax_income >= 800001 && annual_tax_income <= 2000000)
+           {
+               tax_due = (annual_tax_income - 800000);
+                tax_due = (tax_due * 0.25) + 102500 ;
+               cout << "Tax due: " << tax_due << endl;
+ 
+           }
+
+           else if (annual_tax_income == 2000000)
+           {
+               tax_due = annual_tax_income - 402500;
+               cout << "Tax due: " << tax_due << endl;
+ 
+           }
+           else if (annual_tax_income >= 2000001 && annual_tax_income <= 8000000)
+           {
+               tax_due = (annual_tax_income - 2000000);
+               tax_due = (tax_due * 0.30) + 402500 ;
+               cout << "Tax due: " << tax_due << endl;
+           }
+
+           else if (annual_tax_income == 8000000)
+           {
+               tax_due = annual_tax_income - 2202500;
+               cout << "Tax due: " << tax_due << endl;
+ 
+           }
+
+           else if (annual_tax_income >= 8000001)
+           {
+               tax_due = (annual_tax_income - 8000000);
+               tax_due = (tax_due * 0.35) + 2202500 ;
+               cout << "Tax due: " << tax_due << endl;
+           }
+
+
+    }
+
+    if (marital_status == "Married")
+   {      // deduction = 0;
+           sss = monthly_income * 0.05;
+           tax = monthly_income * 0.03;
+           medicare = monthly_income * 0.01;
+           cout << "Your sss is: " << sss << endl;
+           cout << "Your tax is: " << tax << endl;
+           cout << "Your medicare is: " << medicare << endl;
+           cout << "\n Total deduction: " << sss + tax + medicare << endl;
+
+           gross_income = monthly_income - (sss + tax + medicare);
+
+           cout << "Gross income: " << gross_income << endl;
+
+           exemption = dependents * 4000;
+
+           cout << "Exemption: " << dependents * 4000 << endl;
+
+            net_monthly_income = gross_income - exemption;
+
+           cout << "Net monthly income: " << net_monthly_income << endl;
+
+           annual_tax_income = net_monthly_income * 12;
+
+           cout << "Annual tax income: " << annual_tax_income << endl;
+
+           if (annual_tax_income <= 250000)
+           {
+               tax_due = 0;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 250001 && annual_tax_income <= 400000)
+           {
                tax_due = (annual_tax_income - 250000) * 0.20;
                cout << "Tax due: " << tax_due << endl;
            }
@@ -85,19 +182,6 @@ int main()
                tax_due = (annual_tax_income - 8000000) * 0.35;
                cout << "Tax due: " << tax_due << endl;
            }
-
-
-    }
-
-    if (marital_status == "Married")
-   {      // deduction = 0;
-           sss = monthly_income * 0.05;
-           tax = monthly_income * 0.03;
-           medicare = monthly_income * 0.01;
-           cout << "Your sss is: " << sss << endl;
-           cout << "Your tax is: " << tax << endl;
-           cout << "Your medicare is: " << medicare << endl;
-           cout << "\n Total deduction: " << sss + tax + medicare << endl;
        }
 
     if (marital_status == "Widow")
@@ -109,6 +193,53 @@ int main()
            cout << "Your tax is: " << tax << endl;
            cout << "Your medicare is: " << medicare << endl;
            cout << "\n Total deduction: " << sss + tax + medicare << endl;
+
+           gross_income = monthly_income - (sss + tax + medicare);
+
+           cout << "Gross income: " << gross_income << endl;
+
+           exemption = dependents * 4000;
+
+           cout << "Exemption: " << dependents * 4000 << endl;
+
+            net_monthly_income = gross_income - exemption;
+
+           cout << "Net monthly income: " << net_monthly_income << endl;
+
+           annual_tax_income = net_monthly_income * 12;
+
+           cout << "Annual tax income: " << annual_tax_income << endl;
+
+           if (annual_tax_income <= 250000)
+           {
+               tax_due = 0;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 250001 && annual_tax_income <= 400000)
+           {
+               tax_due = (annual_tax_income - 250000) * 0.20;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 400001 && annual_tax_income <= 800000)
+           {
+               tax_due = (annual_tax_income - 400000) * 0.25;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 800001 && annual_tax_income <= 2000000)
+           {
+               tax_due = (annual_tax_income - 800000) * 0.30;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 2000001 && annual_tax_income <= 8000000)
+           {
+               tax_due = (annual_tax_income - 2000000) * 0.32;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 8000001)
+           {
+               tax_due = (annual_tax_income - 8000000) * 0.35;
+               cout << "Tax due: " << tax_due << endl;
+           }
        }
 
        if (marital_status == "HOF")
@@ -120,6 +251,53 @@ int main()
            cout << "Your tax is: " << tax << endl;
            cout << "Your medicare is: " << medicare << endl;
            cout << "\n Total deduction: " << sss + tax + medicare << endl;
+
+           gross_income = monthly_income - (sss + tax + medicare);
+
+           cout << "Gross income: " << gross_income << endl;
+
+           exemption = dependents * 4000;
+
+           cout << "Exemption: " << dependents * 4000 << endl;
+
+            net_monthly_income = gross_income - exemption;
+
+           cout << "Net monthly income: " << net_monthly_income << endl;
+
+           annual_tax_income = net_monthly_income * 12;
+
+           cout << "Annual tax income: " << annual_tax_income << endl;
+
+           if (annual_tax_income <= 250000)
+           {
+               tax_due = 0;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 250001 && annual_tax_income <= 400000)
+           {
+               tax_due = (annual_tax_income - 250000) * 0.20;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 400001 && annual_tax_income <= 800000)
+           {
+               tax_due = (annual_tax_income - 400000) * 0.25;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 800001 && annual_tax_income <= 2000000)
+           {
+               tax_due = (annual_tax_income - 800000) * 0.30;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 2000001 && annual_tax_income <= 8000000)
+           {
+               tax_due = (annual_tax_income - 2000000) * 0.32;
+               cout << "Tax due: " << tax_due << endl;
+           }
+           else if (annual_tax_income >= 8000001)
+           {
+               tax_due = (annual_tax_income - 8000000) * 0.35;
+               cout << "Tax due: " << tax_due << endl;
+           }
        }
        
 
