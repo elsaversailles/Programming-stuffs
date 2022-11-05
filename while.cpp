90ff5ee93f1452affe2d5875f9cca14013b83692
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-11-05 14:09:57 +0800

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -3,22 +3,22 @@ using namespace std;
 int main()
 { 
     double base, exponent, reciprocal, reciprocal1, pw, exponent1;
-    int f,x;
+    int f,x, i;
     cout << "Enter Base: ";
     cin >> base;
     cout << "Enter Exponent: ";
     cin >> exponent;
 
-    //write a loop to calculate the netagive power with given base and exponent
+    //write a loop to calculate the negative power with given base and exponent
     reciprocal = 1/base;
     reciprocal1 = reciprocal;
+
     for (int i = 1; i < exponent; i++)
     {
         reciprocal = reciprocal * reciprocal1;
     }
     pw = reciprocal;
     cout << "answer = " << pw << endl;
-    
  
     exponent1 = exponent*-1;
     while (x<=exponent1)
