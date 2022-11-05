3d17051fd95896e90cac1d50289aa9fc337b42e9
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-11-05 14:00:37 +0800

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    double base, exponent, reciprocal, reciprocal1, pw, exponent1;
+    int f,x;
+    cout << "Enter Base: ";
+    cin >> base;
+    cout << "Enter Exponent: ";
+    cin >> exponent;
+
+    //write a loop to calculate the netagive power with given base and exponent
+    reciprocal = 1/base;
+    reciprocal1 = reciprocal;
+    for (int i = 1; i < exponent; i++)
+    {
+        reciprocal = reciprocal * reciprocal1;
+    }
+    pw = reciprocal;
+    cout << "answer = " << pw << endl;
+    
+ 
+    exponent1 = exponent*-1;
+    while (x<=exponent1)
+    {
+        pw = pw * reciprocal1;
+        x++;
+        cout << "answer = " << pw << endl;
+    }
+
+ 
+
+   
+
+
+}
