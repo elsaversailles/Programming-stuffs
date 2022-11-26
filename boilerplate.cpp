99ca67ad901c9803c0b442e08b1beaa63b988042
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-11-26 10:55:09 +0800

    Update boilerplate.cpp

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -36,3 +36,42 @@ int main() {
 
   return 0;
 }
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{ 
+//ask user to input 5 numbers and display the sum of the numbers
+//and segregrate the positive and negative numbers
+int num, sum=0, userval, sum1, sum2 = 0;
+int pos=0, neg=0;
+// cout<<"Please enter 10 numbers:"<<endl;
+cout<<"Enter number of Values: ";
+cin>>userval;
+for(int i=0; i<userval; i++)
+{
+//check if the number is negative
+cin>>num;
+if(num>0)
+{
+pos++;
+sum1=sum1+num;
+}
+if(num<0)
+//else
+{
+neg++;
+sum2=sum2+num;
+}
+// adding 10 numbers
+sum=sum+num;
+}
+
+cout << "\n The sum of" << userval << "numbers is: "<<sum1 << endl;
+cout << "\n The sum of" << userval << "numbers is: "<<sum2 << endl;
+return 0;
+}
+
+
