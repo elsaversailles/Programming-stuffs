91be2c530d06f764e4292fb7c8d82c1f1c890b35
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-11-26 11:49:50 +0800

    Update loop_final.cpp

--- loop_final.cpp
+++ loop_final.cpp
@@ -4,7 +4,7 @@ int main()
 { 
 //ask user to input 5 numbers and display the sum of the numbers
 //and segregrate the positive and negative numbers
-int num, sum=0, userval, sum1, sum2 = 0;
+int num, sum=0, userval, sum1 = 0, sum2 = 0, sum3 = 0 , sum4 = 0;
 int pos=0, neg=0;
 // cout<<"Please enter 10 numbers:"<<endl;
 cout<<"Enter number of Values: ";
@@ -13,47 +13,58 @@ for(int i=0; i<userval; i++)
 {
 //check if the number is negative
 cin>>num;
-if(num>0)
+if(num>0)//positive
 {
-pos++;
-sum1=sum1+num;
+    if(num%2==0)//even
+    {
+        pos++;
+        sum4=sum4+num;
+    }
+    if(num%2!=0) //odd
+    {
+        pos++;
+        sum2=sum2+num;
+    }
 }
-if(num<0)
-//else
+
+if(num<0)//negative
 {
-neg++;
-sum2=sum2+num;
+    if(num%2==0)
+    {
+        neg++;
+        sum1=sum1+num;
+    }
+ if(num%2!=0) 
+    {
+        neg++;
+        sum3=sum3+num;
+    }
 }
-// adding 10 numbers
 sum=sum+num;
 }
 
-cout << "\n The sum of Positive numbers is: "<<sum1 << endl;
-cout << "\n The sum of Negative numbers is: "<<sum2 << endl;
-if (sum1%2==0)
-{
-   cout << "\n Sum of Positive Even Numbers is: "<<sum1 << endl;
-}
-else
-{
-   cout << "\n Sum of Positive Odd Numbers is: "<<sum1 << endl;
-}
-
-if (sum2%2==0)
-{
-   cout << "\n Sum of Negative Even Numbers is: "<<sum2 << endl;
-}
-else
-{
-   cout << "\n Sum of Negative Odd Numbers is: "<<sum2 << endl;
-}
+// adding 10 numbers
+sum=sum+num;
 
 /*
-cout << "\n Sum of Positive Odd Numbers is: "<<sum1 << endl;
-cout << "\n Sum of Positive Even Numbers is: "<<sum2 << endl;
-cout << "\n Sum of Negative Odd Numbers is: "<<sum1 << endl;
-cout << "\n Sum of Negative Even Numbers is: "<<sum2 << endl;
+cout << "\n The sum of Positive numbers is: "<<sum1 << endl;
+cout << "\n The sum of Negative numbers is: "<<sum2 << endl;
+
+   cout << "\n Sum of Positive Even Numbers is: "<<sum1 << endl;
+
+
+   cout << "\n Sum of Positive Odd Numbers is: "<<sum1 << endl;
+
+   cout << "\n Sum of Negative Even Numbers is: "<<sum2 << endl;
+
+   cout << "\n Sum of Negative Odd Numbers is: "<<sum2 << endl;
+
 */
+
+cout << "\n Sum of Positive Odd Numbers is: "<<sum2 << endl;
+cout << "\n Sum of Positive Even Numbers is: "<<sum4 << endl;
+cout << "\n Sum of Negative Odd Numbers is: "<<sum3 << endl;
+cout << "\n Sum of Negative Even Numbers is: "<<sum1 << endl;
 return 0;
 }
 
