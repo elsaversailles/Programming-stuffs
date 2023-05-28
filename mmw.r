93dd7070a8e68f65865895444d728b9313de8002
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2023-05-28 18:34:18 +0800

    Update mmw.r
    Prod Update

--- mmw.r
+++ mmw.r
@@ -1,3 +1,4 @@
+
 #Pearson Correlation Test
 library(ggplot2)
 cor <- read.csv ("C:\Users\Leny Bondoc\Documents\Book1.csv")
@@ -6,8 +7,21 @@ correlation <- cor(cor$CorrectX, cor$AttitudeY)
 print(correlation)
 
 
+# Print the standard deviation
+print(paste ("Standard Deviation of X: ", sd_x))
+print(paste ("Standard Deviation of Y: ", sd_y))
+#Vanilla Pearson Test
+cor.test(x, y, method = "pearson")
+
+ggplot(cor, aes(x = CorrectX, y = AttitudeY)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(title = "Correlation Plot", x = "Scores", y = "Attitude")
+
 # Read the CSV file into a dataset with a different variable name
 
+
+{
 # Extract the variables
 x <- cor$CorrectX
 y <- cor$AttitudeY
@@ -21,15 +35,6 @@ mean_y <- mean(y)
 model <- lm(y ~ x)
 slope <- coef(model)["x"]
 intercept <- coef(model)["(Intercept)"]
-
-#Graph 
-plot <- ggplot(mydata, aes(x = x, y = y)) +
-  geom_point() +
-  labs(x = "CorrectX", y = "AttitudeY")
-
-# Add the line representing the slope
-plot_with_slope <- plot +
-  geom_abline(intercept = coef(model)["(Intercept)"], slope = slope, color = "red")
 
 
 # Calculate the deviations from the means
@@ -57,49 +62,21 @@ correlation <- sum_product_deviations / sqrt(sum_squared_deviations_x * sum_squa
 #breakdown of correlation
 cor2 <- divisor_multiply1/square
 
-# Print the intermediate values and the correlation coefficient
-print(paste ("Mean of X: ", mean_x))
-
-print(paste ("Mean of Y: ", mean_y))
-
-print("Standard Deviation of X VALUES:")
-print(deviations_x)
-
-print("Standard Deviation of Y VALUES:")
-print(deviations_y)
-
-#numerator
-print (paste ("(X - mean of X)*(y - mean of Y): ", sum_product_deviations))
-
-#Divisor 
-
-#2
-print(paste ("(x - mean of X)^2 : ", sum_squared_deviations_x))
-
-#1
-print(paste ("(y - mean of Y)*(y - mean of Y): ", sum_squared_deviations_y))
-
-#Divisor Method
-print(paste ("Multiply ((x - mean of X)^2) * (y - mean of Y)*(y - mean of Y) : ", divisor_multiply1))
-#Squareroot
-print(paste ("SquareRoot: ", square))
-
-#Pearson Correlation
-print(paste ("Correlation: ", cor2))
-
-print(paste ("Correlation: ", correlation))
-
-#B = slope and Y intercept
-print(paste ("B = Slope: ", slope))
-print(paste("Y intercept: ", intercept))
-
-
 #Plot the Dataset
-plot(x, y, xlab = "CorrectX", ylab = "AttitudeY")
-abline(coef(model), col = "red")
-
-# Print the standard deviation
-print(paste ("Standard Deviation of X: ", sd_x))
-print(paste ("Standard Deviation of Y: ", sd_y))
-
-      
+ggplot(cor, aes(x = CorrectX, y = AttitudeY)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(title = "Correlation Plot", x = "Scores", y = "Attitude") +
+  geom_text(label = paste("Correlation Score:", round(cor(cor$CorrectX, cor$AttitudeY), 4)), x = -Inf, y = Inf, hjust = 0, vjust = 1, color = "black")+
+  geom_text(label = paste("Mean of X:", mean_x), x = -Inf, y = -Inf, hjust = 0, vjust = -39, color = "black")+
+  geom_text(label = paste("Mean of Y:", mean_y), x = -Inf, y = -Inf, hjust = 0, vjust = -38, color = "black")+
+  geom_text(label = paste("(X - mean of X)*(y - mean of Y): ", sum_product_deviations), x = -Inf, y = -Inf, hjust = 0, vjust = -37, color = "black")+
+  geom_text(label = paste("(x - mean of X)^2 : ", sum_squared_deviations_x), x = -Inf, y = -Inf, hjust = 0, vjust = -36, color = "black")+
+  geom_text(label = paste("(y - mean of Y)*(y - mean of Y): ", sum_squared_deviations_y), x = -Inf, y = -Inf, hjust = 0, vjust = -35, color = "black")+
+  geom_text(label = paste("Multiply ((x - mean of X)^2) * (y - mean of Y)*(y - mean of Y) : ", divisor_multiply1), x = -Inf, y = -Inf, hjust = 0, vjust = -34, color = "black")+
+  geom_text(label = paste("SquareRoot: ", round((square), 4)), x = -Inf, y = -Inf, hjust = 0, vjust = -33, color = "black")+
+  geom_text(label = paste("B = Slope: ",  round((slope), 4)), x = -Inf, y = -Inf, hjust = 0, vjust = -32, color = "black")+
+  geom_text(label = paste("SD of X: ", round((sd_x), 4)), x = -Inf, y = -Inf, hjust = 0, vjust = -31, color = "black")+
+  geom_text(label = paste("SD of Y: ", round((sd_y), 4)), x = -Inf, y = -Inf, hjust = 0, vjust = -30, color = "black")
+  
+}
